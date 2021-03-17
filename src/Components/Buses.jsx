afdd47e6cc7f666144d66b8ba23f777ff00b2b16
Miquel Castro <castro-miquel@hotmail.com>
2021-03-17 15:19:21 +0100

    Añado la funcion de ir pasando buses

--- src/Components/Buses.jsx
+++ src/Components/Buses.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Buses = (props) => {
   const { lineas } = props;
 
+  const [posicion, setposicion] = useState(0);
+
+  let i = 0;
+
+  useEffect(() => {
+    setInterval(function () {
+      i++;
+      if (lineas) {
+        if (i < lineas.data.ibus.length) {
+          setposicion(-30 * i);
+        } else {
+          setposicion(0);
+          i = 0;
+        }
+      }
+    }, 2000);
+  }, [lineas]);
+
+
+
+
   return (
     <div className="display">
-      {lineas ? lineas.data.ibus?.map((linea, i) => {
-        return (
-          <div key={i} className="bus">
-            <span className="linea">{linea.line}</span>
-            <span className="destino">{linea.destination}</span>
-            <span className="tiempo">{linea["text-ca"]}</span>
-          </div>
-        );
-      }) : <span className="linea">Cargando o no hay buses </span>}
-    </div>
+      {lineas ?
+        lineas.data.ibus?.map((linea, i) => {
+
+          return (
+            <div key={linea.line} className="bus" style={{ top: posicion }}>
+              <span className="linea">{linea.line}</span>
+              <span className="destino">{linea.destination}</span>
+              <span className="tiempo">{linea["text-ca"]}</span>
+            </div>
+          );
+        })
+        : <span className="linea">Cargando o no hay buses </span>}
+    </div >
   );
+
+
+
 };
 
 export default Buses;
