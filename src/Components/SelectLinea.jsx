b0924a2a1b376dbe9284b48ef87a8ccc9257f434
Miquel Castro <castro-miquel@hotmail.com>
2021-03-18 19:40:21 +0100

    Arreglo el warning del select añadiendo una variable de estado y poniendo el valor de la variable en el select

--- src/Components/SelectLinea.jsx
+++ src/Components/SelectLinea.jsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import React from "react";
 
 const SelectLinea = (props) => {
   const { lineas, tiempoLineaHandler } = props;
+  const [lineaSelec, setLineaSelect] = useState("");
 
-  const obtenirLineaSeleccionada = (e) => {
-    tiempoLineaHandler(e.target.value);
+  const obtenirLineaSeleccionada = (valor) => {
+    setLineaSelect(valor);
+    tiempoLineaHandler(valor);
   };
 
   return (
     <form>
       <label htmlFor="tiempo-linea">Tiempo para que llegue la línea: </label>
-      <select onChange={obtenirLineaSeleccionada} id="tiempo-linea">
-        <option disabled selected value>Elige línea</option>
+      <select value={lineaSelec} onChange={(e) => obtenirLineaSeleccionada(e.target.value)} id="tiempo-linea">
+        <option disabled value="">Elige línea</option>
         {lineas ?
           lineas.data.ibus?.map((linea) => {
             return (
